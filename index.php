4a5129c69e8b1b336e56f76856361ade8fb0c60c
ValdoMoraisFlor <valdoMorais.vm@gmail.com>
2021-06-02 17:55:36 +0200

    footer1

---
+++ index.php
@@ -0,0 +1,304 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <link rel="shortcut icon" href="imagens/hbb.png" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="HCB.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap" rel="stylesheet">
+<title>Hospital Central da Beira</title>
+
+</head>
+
+<header>
+
+  <div id="menu-area">
+      <div class= "logo">
+          <a href="#">
+              <img class="img1" src="imagens/hbb.png"  alt="">
+          </a>
+        </div>
+        <ul>
+            <li><a href="#principal">Inicio</a></li>
+            <li><a href="#services">Serviços</a></li>
+            <!--<li><a href="#">Formação</a></li>-->
+            <li><a href="#">Doação</a></li>
+            <li><a href="#imprensa">Imprensa</a></li>
+            <li><a href="#">Contactos</a></li>
+        </ul>    
+    </div>
+    <div class="banner-area">
+        <div class="banner-text">
+            <h2><a id="principal"></a>O nosso maior valor e a vida </h2>
+            <div id="dv">
+                <a href="#">Mais detalhes</a>
+            </div>
+        </div>
+    </div> 
+</header>
+<body>   
+    <div class="contitle">
+        <div class="inner1">
+
+            <h2><a id="services"></a>Serviços de Apoio Clínico</h2>
+            <div class="inner1A">
+            </div>
+        </div>
+    </div>
+    <div class="containerr">
+        <div class="box" id="box1"><h6 class="flexTitle"><a href="#" class="flexlink">Medicina Legal</a><div id="fechar" align=right></div></h6><p>O Serviço de Medicina Legal
+             e Seguros do Hospital Central de Maputo, 
+        é um serviço com características próprias que tem por objectivo auxiliar a esfera do Direito,
+         sem a qual não se concebe uma correcta administração da Justiça. Surgiu da necessidade de solucionar questões judiciais ou<a id="lerrma" >…Ler mais</a></p> </div>
+	
+
+
+        <div class="box" id="box2"><h6 class="flexTitle"><a href="#" class="flexlink">Anatomia patológica</a></h6><p>O Serviço de Anatomia Patológica 
+            (SAP) é responsável pela realização
+             do estudo morfológico de órgãos, tecidos e células de doentes vivos  (através de amostras obtidas por biópsias, peças cirúrgicas ou colheita de células de vários tipos), ou de falecidos por doença (através da autópsia) <a href="#">…Ler mais</a></p></div>
+        <div class="box"><h6 class="flexTitle"><a href="#" class="flexlink">Laboratório de análises clinicas</a></h6><p>O Laboratório de
+             Análises Clínicas (LAC) é uma rede 
+            de laboratórios composta por quatro laboratórios de rotina (Bioquímica, Hematologia,  Microbiologia, Biologia Molecular) que inclui a Sala de Colheita e Recepção de amostras e três Laboratórios de Urgências (Laboratório de Urgência de Pediatria.  <a href="#">…Ler mais</a></p></div>
+        <div class="box"><h6 class="flexTitle"><a href="#" class="flexlink">Dor e cuidados paliativos</a></h6><p>A Unidade da Dor e Cuidados Paliativos
+             é um Serviço que presta cuidados de saúde a doentes com dor crónica oncológica  em ambulatório ou hospitalizados no Hospital Central de Maputo.  <a href="#">…Ler mais</a></p></div>
+        <div class="box"><h6 class="flexTitle"><a href="#">
+            Urgências de Adultos (Ambulatório, Traumatologia, SO)</a></h6><p>O Serviço de Urgência de Adultos do Hospital Central de Maputo, 
+            é um serviço vocacionado ao atendimento de doentes de todos os foros de carácter <a href="#">…Ler mais</a></p></div>
+        <div class="box" ><h6 class="flexTitle"><a href="#">Cuidados Intensivos</a></h6><p>A Unidade de Cuidados Intensivos (U.C.I) é um serviço localizado 
+            dentro do Serviço de Urgências que admite pacientes críticos em condições fisiopatológicas que ameaçam a <a href="#">…Ler mais</a></p></div>
+        <div class="box"><h6 class="flexTitle"><a href="#">Radiologia</a></h6><p>O Serviço de Radiologia e Imagiologia é uma área médica dedicada ao auxílio diagnostico, 
+            tratamento e intervenção na base de várias técnicas de obtenção de imagem e oferece exames <a href="#">…Ler mais</a></p></div>
+        <div class="box"><h6 class="flexTitle"><a href="#">Bloco Operatório</a></h6><p>Os Serviços operatórios comportam 03 blocos nomeadamente: Bloco Operatório Central 
+            (A e B) com 9 salas e 14 mesas, Bloco Operatório da Maternidade, com 4 salas e 4 mesas <a href="#">…Ler mais</a></p></div>
+        </div>
+        <div class="newsdiv">
+            <div class="newsindiv">
+                <h2><a id="imprensa"></a>Noticias</h2>
+                <div class="underline">
+
+                </div>
+            </div>
+        </div>
+    <section class="container1">
+        <div class="card">
+            <div class="card-image car-1"></div>
+            <h2>Luta Contra a Covid-19</h2>
+            <p>A COVID-19 afeta cada pessoa de formas diferentes. A maioria das pessoas infetadas desenvolve a doença com sintomas ligeiros a moderados e recupera sem necessidade de hospitalização.
+                Sintomas mais comuns:febre, tosse seca, cansaço
+                Sintomas menos comuns: tensão e dores musculares, dores de garganta, diarreia, conjuntivite, dor de cabeça, perda de paladar ou olfato, irritações na pele ou descoloração dos dedos das mãos ou dos pés.</p>
+            
+            <a href="#">LER MAIS</a>
+        </div>
+        <div class="card">
+            <div class="card-image car-2"></div>
+            <h2>Novos Equipamentos no HCB</h2>
+            <p> O otoscópio é o aparelho hospitalar mais importante para a realização de exames na parte interna do ouvido.<br>
+                O oxímetro foi desenvolvido para a medição da saturação de oxigênio no sangue. É usado para indicar problemas em qualquer especialidade, especialmente no diagnóstico de distúrbios respiratórios.</p>
+            
+            <a href="#">LER MAIS</a>
+        </div>
+        <div class="card">
+            <div class="card-image car-3"></div>
+            <h2>Medicos Cirugiões Na sala</h2>
+            <p>Cardiologia
+                é o especialista nas doenças do coração e seu sistema circulatório, realizando o diagnóstico e promovendo o tratamento.
+                <br>
+                Dermatologia
+                cuida da prevenção e tratamento das doenças do maior órgão do corpo: a pele e seus apêndices, como a boca, os cabelos, as unhas etc</p>
+            <a href="#">LER MAIS</a>
+        </div>
+    </section>            
+
+    <div class="contitle">
+        <div class="inner1">
+            <h2>Doações</h2>
+            <div class="inner1A"></div>
+        </div>
+    </div><br><br>
+   
+    
+        <div class="popup" id="popup-1">
+            <div class="overlay"></div>
+            <div class="content">
+                <div class="close-btimagens/HCB.gifn" onclick="togglePopup()"> &times</a></div>
+                <h1>Title</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto cum ducimus modi eligendi alias et corporis. Possimus iusto incidunt soluta eligendi doloribus ducimus fugiat quisquam id vero corporis? Necessitatibus provident animi dicta odio in architecto magni, enim ducimus, nostrum eos quia asperiores tenetur dignissimos nemo a molestiae atque cupiditate optio?</p>
+            </div>
+
+    <section class="container">
+       
+       <div id="donate">
+            <div id="indonate">
+                <div class="indonatea" id="don1"><div id="backgroundia"><h2>Sangue</h2>
+                <p>A doação de sangue é algo muito importante para o funcionamento do hospital pois pode evitar que os doentes que necessitam de sangue esperem por alguem que</p></div></div>
+                <div class="indonateb"><div id="backgroundia"><h2>Leite Materno</h2><p>A doação do leite materno é também importante para garantir a alimentação dos bebés menores de 6 meses cuja mae não está presente ou por algum motivo não possa amamentar </p></div></div>
+            </div>
+            <div id="indonate2">
+                <div class="indonate2a"><div id="backgroundia"><h2>Roupa</h2><p>A doacao de roupa é parte do departamento da ajuda humanitaria para ajudar aos mais carenciados</p></div></div>
+                <div class="indonate2b"> <div id="backgroundia"><h2>Alimentos</h2><p>A doação de alimentos pode ser tambem é feita para ajudar aos mais carenciados a conseguirem algum alimento, no entanto cada um de nos deve doar alimentos não pereciveis</p></div></div>
+            </div>
+
+       </div>
+       <div id="divdonatetable">
+           <table  >
+               <tr>
+                   <th class="th1">Horas</th>
+                   <th class="th1">Dias</th>
+                   <th class="th2">Doacao-local</th>
+               </tr>
+               <tr>
+                    <td class="th1">12:00H-18:00H</td>
+                    <td class="th1">Todos os dias</td>
+                    <td class="th2">Doação de sangue - Banco de sangue</td>   
+               </tr>
+               <tr>
+                   <td class="th1">08:30h-18:00h</td>
+                   <td class="th1">segunda - quinta-feira</td>
+                   <td class="th2">Doação de Alimentos- Dep de ajuda Humanitaria</td>
+               </tr>
+               <tr>
+                   <td class="th1">10:30h-18:30</td>
+                   <td class="th1">Segunda- Sabado</td>
+                   <td class="th2">Leite maternno- Banco de leite</td>
+               </tr>
+               <tr>
+                   <td class="th1">14:00h-18:00h</td>
+                   <td class="th1">segunda - sexta-feira</td>
+                    <td class="th2">Roupa-dep de ajuda humanitaria</td>
+               </tr>
+            </table>
+       </div>
+    </section><br><br>
+
+    <!--contactos part-->
+    <div class="newsdiv">
+        <div class="newsindiv">
+            <h2><a id="imprensa"></a>Contactos</h2>
+            <div class="underline">
+
+            </div>
+        </div>
+    </div>
+
+    <div class="body">
+        <div class="containercont">
+          <div class="text">
+  Contacte nos</div>
+  <form action="#">
+          <div class="form-row">
+            <div class="input-data">
+              <input type="text" required>
+              <div class="underline">
+  </div>
+  <label for="">First Name</label>
+            </div>
+  <div class="input-data">
+              <input type="text" required>
+              <div class="underline">
+  </div>
+  <label for="">Last Name</label>
+            </div>
+  </div>
+  <div class="form-row">
+            <div class="input-data">
+              <input type="text" required>
+              <div class="underline">
+  </div>
+  <label for="">Email Address</label>
+            </div>
+  
+            <div class="input-data textarea">
+  
+              <textarea rows="8" cols="80" required></textarea> 
+              <br />
+  <div class="underline"></div>
+  <label for="">Write your message</label>
+            
+  <div class="form-row submit-btn">
+            <div class="input-data">
+              <div class="inner"></div>
+              <input type="submit" value="send">
+            </div>
+          </div>
+        </form>
+      </div>
+      </div><br><br>
+     
+            <script>
+                window.addEventListener("scroll", function(){
+                    let menuArea = document.getElementById('menu-area');
+
+                        if(window.pageYOffset > 0){
+                            menuArea.classList.add("cus-nav");
+                        }else{
+                            menuArea.classList.remove("cus-nav")
+                        }
+                })
+
+                var a = window.document.querySelector('#don1') 
+                    a.addEventListener('mouseover',entrar)
+                        function entrar(id){
+                        document.getElementById(Id).style.display='block'
+                    }
+
+                   // function entrar(){
+                     //   a.innerHTML.style.display="block"
+                     //   document.getElementById('backgroundia').style.display="block"
+                    //}
+
+                function abrir(id){
+                    document.getElementById(id).style.display='block';
+                    a = box1.innerHTML
+                    posiciona.innerHTML = a 
+
+                    b = img.src='imagens/xout.png';
+                    getElementById('fechar') = b;
+                  }
+                function fechar(id){
+                    document.getElementById("posiciona").style.display="none";
+                }
+                function togglePopup(){
+                document.getElementById("popup-1").classList.toggle("active");
+          
+        }
+            </script>
+        </div>
+    </div>
+    <Footer>      
+       <div class="content1">
+           <h3>Hospital Central da Beira</h3><br>
+           <p>O Hospital Central da Beira (HCB) é a segunda maior unidade hospitalar de Moçambique. É a que dispõe de melhores recursos de trabalho do ponto de vista de equipamento e profissionais especializados. É, igualmente, a que recebe maior número de doentes que sofrem das mais variadas enfermidades.</p>
+
+          <div class="social">
+               
+            </div>
+        </div>
+        <div class="footerBtn">
+            <p>Copyright &copy;2021.</p>
+        </div>
+           <!-- <div class="Cbox">
+                <h2>Address</h2>
+                <br>
+                <div class="content11">
+                    <div class="place">
+                        <span class= "fas fa-map-marker-alt"></span>
+                        <span class="text"> Macuti, Av.FPLM</span>
+                    </div>
+                    <div class="phone">
+                        <span class="fas fa-phone-alt"></span>
+                        <span class="text"> +258-8467777776</span>
+                    </div>
+                    <div class="email">
+                        <span class="fas-envelope"> </span>
+                        <span class="text">HCBeira@gmail.com</span>
+                    </div>
+                </div>
+            </div>
+        </div>-->   
+    </Footer>
+
+</body>
+</html>
